4d183e19870829b57ad1aa611b4bad7370e82ecd
ColeGuion <coletplc@gmail.com>
2026-01-26 19:31:07 -0600

    Remove json-c

--- gec-demo/src/internal/gec/gec.go
+++ gec-demo/src/internal/gec/gec.go
@@ -11,7 +11,7 @@ package gec
 #cgo LDFLAGS: -L${SRCDIR}/../../native/gec_runtime/third_party/onnxruntime/lib
 #cgo LDFLAGS: -L${SRCDIR}/../../native/gec_runtime/third_party/sentencepiece/lib
 #cgo LDFLAGS: -L${SRCDIR}/../../native/gec_runtime/third_party/icu/lib
-#cgo LDFLAGS: -lonnxruntime -lsentencepiece -lstdc++ -lm -ldl -ljson-c -licuuc -licudata
+#cgo LDFLAGS: -lonnxruntime -lsentencepiece -lstdc++ -lm -ldl -licuuc -licudata
 
 #include "inference.h"
 #include <stdbool.h>
